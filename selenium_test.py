9141f55c57ee59eadd1c5173fcbccf72b4c281ad
CaDelTo <90171730+CaDelTo@users.noreply.github.com>
2025-01-29 20:00:50 -0500

    Add Selenium headless test script and update .gitignore

---
+++ selenium_test.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+#Configurar Selenium en modo headless (sin interfaz gráfica)
+options = Options()
+options.add_argument("--headless")  # Ejecutar en segundo plano
+options.add_argument("--disable-gpu")  # Evitar errores con la GPU
+options.add_argument("--disable-software-rasterizer")  # Evita uso de software GPU
+options.add_argument("--no-sandbox")  # Evita problemas en entornos Linux
+options.add_argument("--disable-dev-shm-usage")  # Evita errores de memoria compartida
+
+# Configurar Selenium con Chrome
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+# Abrir la página
+url = "https://www.colombiaenmapas.gov.co/#"
+driver.get(url)
+
+# Esperar a que el contenido dinámico cargue
+# Esperar a que el contenido dinámico cargue
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "panel-resultados-titulo2")))
+
+# Extraer elementos específicos
+elementos = driver.find_elements(By.CLASS_NAME, "panel-resultados-titulo2")  # Ajusta el selector CSS
+
+# Mostrar el texto extraído
+for elemento in elementos:
+    print(elemento)
+
+# Cerrar el navegador
+driver.quit()
