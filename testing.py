6b725c37c98c7fe564337c0bcbb8f0f548ecb2f8
Cadeltoro <cadeltoro@uninorte.edu.co>
2025-03-20 20:29:47 -0500

    Add data retrieval and querying strategies for WFS, WMS, and ArcGIS; implement DataWebQuerier class and related functions

---
+++ testing.py
@@ -0,0 +1,7 @@
+class DataRetriver:
+    def get(self, source, polygon):
+        return 'data'
+    def add(self, a, b ):
+        return a + b
+    def update(self, a, b):
+        return a - b
---
+++ web_querier/strategies/base_strategy.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class BaseStrategy(ABC):
+    @abstractmethod
+    def get_data(self, url, polygon):
+        pass
